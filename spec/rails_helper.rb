d5096ff1122cddbd5d3f12f43973be9aa494c9b0
Kohei Yamamoto <kymmt90@gmail.com>
2017-09-23 17:42:10 +0900

    Set up for writing request specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+require 'support/factory_girl'
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -15,6 +17,12 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-end
 
-require 'support/factory_girl'
+  config.include RSpec::DefaultHttpHeader
+  config.include RSpec::JsonMatcher
+
+  config.before :example, type: :request, json: true do
+    default_headers['CONTENT_TYPE'] = 'application/json'
+    default_headers['ACCEPT'] = 'application/json'
+  end
+end
