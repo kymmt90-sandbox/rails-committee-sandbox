7b5ed5fb6dd709a18eb2e881d162f45f0ce9e419
Kohei Yamamoto <kymmt90@gmail.com>
2017-10-08 17:21:12 +0900

    Realize as reopening Railtie

--- lib/watcher.rb
+++ lib/watcher.rb
@@ -1,22 +1,27 @@
-class Railties < ::Rails::Railtie
-  initializer '' do |app|
-    watcher = ActiveSupport::FileUpdateChecker.new([], { Rails.root.join('docs').to_s => ['.json'] }) do
-      Rails.logger.info 'updated'
-      # begin
-      #   OpenApiParser::Specification.resolve(
-      #     Rails.root.join('docs', 'swagger.json'), validate_meta_schema: true
-      #   )
-      #   puts 'OpenAPI doc has been successfully validated.'
-      # rescue JsonSchema::ValidationError => e
-      #   warn 'OpenAPI doc generation failed.'
-      #   Rails.logger.error e.full_message
-      # end
-    end
-    app.reloaders << watcher
-    config.to_prepare do
-      puts 'prepare'
-      puts watcher.updated? ? 'updated' : 'not updated'
-      watcher.execute_if_updated
+require 'json'
+require 'open_api_parser'
+
+module Watcher
+  class Railtie < ::Rails::Railtie
+    initializer 'open api' do |app|
+      docs_paths = Dir.glob("docs/*.json")
+      open_api_reloader = ActiveSupport::FileUpdateChecker.new(docs_paths) do
+        begin
+          OpenApiParser::Specification.resolve(
+            Rails.root.join('docs', 'swagger.json'), validate_meta_schema: true
+          )
+          puts 'OpenAPI doc has been successfully validated.'
+        rescue JsonSchema::AggregateError, JsonSchema::SchemaError, JsonSchema::ValidationError => e
+          warn 'OpenAPI doc generation failed.'
+          warn e.to_s
+        end
+      end
+
+      app.reloaders << open_api_reloader
+
+      config.to_prepare do
+        open_api_reloader.execute_if_updated
+      end
     end
   end
 end
